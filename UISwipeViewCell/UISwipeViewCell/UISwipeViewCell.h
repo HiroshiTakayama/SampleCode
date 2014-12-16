30d315295527c2694d71b74b13e03431272db2cc
HiroshiTakayama <takayama@takayamahiroshi-no-MacBook-Air.local>
2014-12-16 21:10:38 +0900

    2st commit

---
+++ UISwipeViewCell/UISwipeViewCell/UISwipeViewCell.h
@@ -0,0 +1,13 @@
+//
+//  UISwipeViewCell.h
+//  UISwipeViewCell
+//
+//  Created by Takayama on 2014/12/15.
+//  Copyright (c) 2014年 Hiroshi Takayama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UISwipeViewCell : NSObject
+
+@end
---
+++ UISwipeViewCell/UISwipeViewCell/UISwipeViewCell.m
@@ -0,0 +1,13 @@
+//
+//  UISwipeViewCell.m
+//  UISwipeViewCell
+//
+//  Created by Takayama on 2014/12/15.
+//  Copyright (c) 2014年 Hiroshi Takayama. All rights reserved.
+//
+
+#import "UISwipeViewCell.h"
+
+@implementation UISwipeViewCell
+
+@end
